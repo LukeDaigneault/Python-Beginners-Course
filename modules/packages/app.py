19e775c0a32815119b8101dc5d5caff5aebd128a
Luke Daigneault <luke.daigneault@gmail.com>
2019-10-29 22:44:27 +0100

    Modules section

---
+++ modules/packages/app.py
@@ -0,0 +1,14 @@
+import ecommerce.sales
+
+from ecommerce.sales import calc_tax, calc_shipping
+
+from ecommerce import sales
+
+ecommerce.sales.calc_shipping()
+ecommerce.sales.calc_tax()
+
+sales.calc_shipping()
+sales.calc_tax()
+
+calc_shipping()
+calc_tax()
